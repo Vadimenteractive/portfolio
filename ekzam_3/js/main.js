62de0fdb39b5b5947972e0d287300f1d9f395e92
vadimenteractive <vadimenteractive@mail.ru>
2018-02-18 00:46:50 +0200

    ekzam3

---
+++ ekzam_3/js/main.js
@@ -0,0 +1,33 @@
+;(function($){
+    $(function () {
+        $('.isotope_1').isotope({
+            layoutMode: 'packery',
+            itemSelector: '.rectangle'
+        });
+        $('.carousel').carousel({
+            interval: 5000
+        });
+        $('.slide-contein').slick({
+            autoplay: true,
+            autoplaySpeed: 2000,
+            infinity: true,
+            dots: false,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            responsive:[
+                {
+                    breakpoint: 960,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 728,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        })
+    })
+})(jQuery);
